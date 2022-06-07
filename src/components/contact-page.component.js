b7d0374a2b9f0f42f3d3f06d4c4a99829514f7b3
Robby Bush <crobertbush@gmail.com>
2022-06-07 15:39:37 -0400

    update

--- src/components/contact-page.component.js
+++ src/components/contact-page.component.js
@@ -41,7 +41,9 @@ export default class ContactPage extends Component {
       </div>
 
       <div>
-        <h2 style={styles.number}>Telephone: (555)555-5555</h2>
+        <h2 style={styles.number}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="28" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg> (555)555-5555</h2>
       </div>
       
   </div> 
