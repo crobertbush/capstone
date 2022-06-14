2c5ac575a004b4ed9b64662330cb541aa7789eb7
Robby Bush <crobertbush@gmail.com>
2022-06-14 15:17:36 -0400

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 
 require('dotenv').config();
@@ -32,11 +32,11 @@ const emailRouter = require('./routes/email');
 
     if (process.env.NODE_ENV === "production"){
       app.use(express.static('client/build'))
-      app.get('/*', (req, res) => {
+      app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build'))
       })
     };
-    
+
 app.listen(port, () => {
 console.log(`Server is running on port: ${port}`);
 });
