19db5ee8b527e397c447c59aa8cf79870e187d8c
Robby Bush <crobertbush@gmail.com>
2022-06-14 15:29:09 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
-
+const path = require('path');
 
 require('dotenv').config();
 
