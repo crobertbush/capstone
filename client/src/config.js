f635194ff2f44a85694b8b7d375a11647372294c
Robby Bush <crobertbush@gmail.com>
2022-06-14 15:44:29 -0400

    push

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,4 @@
 import dotenv from 'dotenv'
 dotenv.config()
 export const BACKEND_URL=process.env.NODE_ENV === 'development' ? "http://localhost:5000/" :  "https://kable-capstone-project.herokuapp.com/"
+// comment
